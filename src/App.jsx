749913a922860d396cf152573680a633d0f5fbba
kpovel <kar.pavel123@gmail.com>
2022-06-29 23:48:57 +0300

    rewrite the form and cleanup code

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,65 @@
 import './App.css';
 import React from "react";
 
-function TextInput() {
-  return (
-   <input type="text" name="gender" id="input" autoFocus={true}/>
-  )
+function TextInput(props) {
+  return <input value={props.this.state.value} onChange={props.this.handleChange} type="text" autoFocus={true}/>
 }
 
 function Button() {
-  return (
-   <button type="submit" id="button">getGender</button>
-  )
+  return <button type="submit">getGender</button>
 }
 
 function Result(props) {
-  return (
-   <div>{props.result}</div>
-  )
+  return <div>{props.result}</div>
 }
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {result: null}
-    this.saveName = this.saveName.bind(this);
+    this.state = {value: '', result: ''};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
-  saveName() {
-    this.setState(() => {
-      return {name: document.getElementById('input').value};
+  handleChange(event) {
+    this.setState( {
+      value: event.target.value
     })
   }
   
-  handleEnterEvent(e) {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      
-      this.sendRequest();
-    }
-  }
-  
-  handleClickEvent(e) {
+  async handleSubmit(e) {
     e.preventDefault();
+    this.setState({
+      value: ''
+    })
     
-    this.sendRequest();
-  }
-  
-  async sendRequest() {
     const SERVER_URL = 'https://api.genderize.io';
-    const firstName = this.state.name;
+    const firstName = this.state.value;
     const url = `${SERVER_URL}?name=${firstName}`;
     
     const response = await fetch(url);
     const result = await response.json();
-    console.log(result);
+    
     this.showResult(result);
   }
   
   showResult(user) {
-    this.setState({
-      result: `${user.name} - ${user.gender}`
+    this.setState(() => {
+      if (user.name.length < 3) {
+        return {result: 'Error'};
+      }
+      return {result: `${user.name} - ${user.gender}`};
+      
+      //  todo: redone error output
     });
   }
   
   render() {
     return (
-     <div onChange={this.saveName}>
-       <form onKeyDown={this.handleEnterEvent.bind(this)}>
-         <TextInput/>
-       </form>
-       <form onClick={this.handleClickEvent.bind(this)}>
+     <div>
+       <form onSubmit={this.handleSubmit}>
+         <TextInput this={this}/>
          <Button/>
        </form>
        <Result result={this.state.result}/>
